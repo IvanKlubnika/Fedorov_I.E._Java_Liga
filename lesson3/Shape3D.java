5875713a47a913c2c8ef01958b40d01fbf2f2223
IvanKlubnika <63202835+IvanKlubnika@users.noreply.github.com>
2020-10-21 03:07:58 +0300

    Update and rename RectangularShapes.java to Shape3D.java

---
+++ lesson3/Shape3D.java
@@ -0,0 +1,9 @@
+package com.bad.code;
+
+//Интерфейс для трехмерных фигур
+public interface Shape3D {
+   Double getLength();
+   Double getWidth();
+   Double getHeight();
+   Double getVolume();
+}
